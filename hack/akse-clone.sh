513d97b46c919fa68a88afabb2b65d5f44b9e5a7
jadarsie <jadarsie@microsoft.com>
2020-04-21 22:51:49 -0700

    skip clone if done

--- hack/akse-clone.sh
+++ hack/akse-clone.sh
@@ -8,6 +8,11 @@ set -o xtrace
 REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 cd "${REPO_ROOT}" || exit 1
 
+if [ -d aks-engine ]; then 
+    (cd aks-engine && git fetch && git reset --hard origin/${AKSE_BRANCH})
+    return
+fi
+
 git clone https://github.com/${AKSE_REPO} -b ${AKSE_BRANCH}
 
 if [[ "${OSTYPE}" == "linux-gnu" ]]; then
